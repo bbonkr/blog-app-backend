eaf4ebbd32b2807eb33d6960a3546fdae681ee81
Pon Cheol, Ku (구본철) <bbon@bbon.kr>
2020-12-13 15:13:23 +0900

    Add Tag entity, fixed AB#155

---
+++ src/MangoBlog.Entities/Tag.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoBlog.Entities
+{
+    public class Tag
+    {
+        /// <summary>
+        /// Identifier
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Tag name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Url friendly tag name
+        /// </summary>
+        public string Slug { get; set; }
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        public string Description { get; set; }
+
+        public bool IsEnabled { get; set; } = true;
+
+        public bool IsDeleted { get; set; } = false;
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        public DateTimeOffset? DeletedAt { get; set; }
+    }
+}
