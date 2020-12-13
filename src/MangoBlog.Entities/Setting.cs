0fb8b40dcd821735fc268773bef461979d6c9662
Pon Cheol, Ku (구본철) <bbon@bbon.kr>
2020-12-13 16:03:30 +0900

    AB#161

---
+++ src/MangoBlog.Entities/Setting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoBlog.Entities
+{
+    public class Setting : EntityBase
+    {
+        /// <summary>
+        /// Allow media file extension. 
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Whitelist
+        /// </para>
+        /// <para>Comma separated. </para>
+        /// <para>e.g.)<code>.png,.jpg,.gif</code></para>
+        /// </remarks>
+        public string AllowMediaExtensions { get; set; }
+    }
+}
