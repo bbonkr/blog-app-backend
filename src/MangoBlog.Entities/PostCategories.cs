bc711eae6b748777533f0389096dbf6d240c2f2a
Pon Cheol, Ku (구본철) <bbon@bbon.kr>
2020-12-13 16:03:05 +0900

    AB#151, AB#154

---
+++ src/MangoBlog.Entities/PostCategories.cs
@@ -0,0 +1,13 @@
+﻿namespace MangoBlog.Entities
+{
+    public class PostCategories
+    {
+        public string PostId { get; set; }
+
+        public string CategoryId { get; set; }
+
+        public virtual Post Post { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
